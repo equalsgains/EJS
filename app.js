e30f985ba082e0b4454cb1e08bc1df9b468fe5e2
Luis Quintanilla <equalsgains@gmail.com>
2017-05-06 09:06:02 -0600

    learned about partials, and how to add style to ejs templetates

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 var express = require("express");
 var app = express();
-
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 
 
 app.get("/", function(req, res){
-    res.render('home.ejs');
+    res.render('home');
 });
 
 
 app.get("/fallinlovewith/:thing", function(req, res){
    var thing = req.params.thing;
-   res.render("love.ejs", {thingVar: thing});
+   
+   res.render("love", {thingVar: thing});
 });
 
+app.get("/posts", function (req, res) {
+   var posts = [
+       {
+           title: "post1",
+           author: "luis"
+       },
+       {
+           title: "post2",
+           author: "mom"
+       },
+       {
+           title: "post3",
+           author: "dad"
+       }
+   ];
+   res.render("posts", {posts: posts});
+});
 
 app.get("*", function(req, res){
     res.send("hey");
